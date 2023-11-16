cb6042dd061d3a3bf6589d421cd27316725c0dc3
212118 <212118@1305-013>
2023-11-16 18:45:17 +0300

    added authorizate page

---
+++ Uchebnaya_Azaliya/Uchebnaya_Azaliya/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Uchebnaya_Azaliya.Base;
+
+namespace Uchebnaya_Azaliya
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Uchebka_SSEntities db = new Uchebka_SSEntities();
+        public static bool IsAdmin = false;
+        public static bool IsTeacher = false;
+    }
+}
